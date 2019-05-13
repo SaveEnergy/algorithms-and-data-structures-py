fad9569738d160205d972c614cf6e15b0d66eba4
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-05-13 09:25:45 +0200

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,11 +1,11 @@
 
 def binary_search_rec(x: int, a, pos, end):
-    if pos > end:
-        return -1
-    
+
     mid = (pos + end) // 2
 
-    if a[mid] == x:
+    if pos > end:
+        return -1
+    elif a[mid] == x:
         return mid
     elif a[mid] < x:
         return binary_search_rec(x, a, mid + 1, end)
@@ -16,7 +16,7 @@ def binary_search_rec(x: int, a, pos, end):
 def binary_search(x: int, a):
     pos = 0
     end = len(a) - 1
-    binary_search_rec(x, a, pos, end)
+    return binary_search_rec(x, a, pos, end)
 
 
-print(binary_search(4, [1, 2, 3, 4, 5, 6, 7, 8]))
+print(binary_search(4, [1, 1, 2, 3, 4, 5]))
