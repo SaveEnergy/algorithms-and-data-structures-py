d3bdef6cc6ed044c7dfab05665a00d3f6662920e
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-05-17 08:59:58 +0200

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,15 +3,15 @@ import random
 rand_data = [random.randint(0, 100) for i in range(10)]
 
 
-def swap(x: int, array: list):
+def shift(x: int, array: list):
     array[x], array[x+1] = array[x+1], array[x]
 
 
-def bubble_sort(array: list):
+def bubble_sort_itr(array: list):
     for i in range(len(array) - 1):
         for j in range(len(array) - i - 1):
             if array[j] > array[j + 1]:
-                swap(j, array)
+                shift(j, array)
     return array
 
 
@@ -21,7 +21,7 @@ def bubble_sort_rec(array: list, n: int):
     else:
         for i in range(n - 1):
             if array[i] > array[i + 1]:
-                swap(i, array)
+                shift(i, array)
                 bubble_sort_rec(array, n - 1)
         return array
 
@@ -31,4 +31,4 @@ print(rand_data)
 print("\nBubblesort rekursiv:")
 print(bubble_sort_rec(list(rand_data), len(rand_data)))
 print("\nBubblesort iterativ:")
-print(bubble_sort(list(rand_data)))
+print(bubble_sort_itr(list(rand_data)))
